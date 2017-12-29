713031b802a7c92b95ef63a9edb47b11a8e810d7
Diwaker Ghimire <idwaker@gmail.com>
2017-12-29 09:33:33 +0545

    added base models

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    fullname = models.CharField(max_length=128)
+    address = models.TextField()
+    phone = models.CharField(max_length=32)
+    email = models.CharField(max_length=128)
+
+    def __repr__(self):
+        return self.fullname
+
+    __str__ = __repr__
+
+
+class Doctor(models.Model):
+    fullname = models.CharField(max_length=128)
+    address = models.TextField()
+    phone = models.CharField(max_length=32)
+    email = models.CharField(max_length=128)
+    specialty = models.CharField(max_length=128)
+    department = models.CharField(max_length=128)
+
+    def __repr__(self):
+        return self.fullname
+
+    __str__ = __repr__
+
+
+class Clinic(models.Model):
+    name = models.CharField(max_length=128)
+    address = models.TextField()
+    phone = models.CharField(max_length=32)
+    email = models.CharField(max_length=128)
+
+    def __repr__(self):
+        return self.name
+
+    __str__ = __repr__
+
+
+class Lab(models.Model):
+    name = models.CharField(max_length=128)
+    address = models.TextField()
+    phone = models.CharField(max_length=32)
+    email = models.CharField(max_length=128)
+
+    def __repr__(self):
+        return self.name
+
+    __str__ = __repr__
+
+
+class Pharmacy(models.Model):
+    name = models.CharField(max_length=128)
+    address = models.TextField()
+    phone = models.CharField(max_length=32)
+    email = models.CharField(max_length=128)
+
+    def __repr__(self):
+        return self.name
+
+    __str__ = __repr__
