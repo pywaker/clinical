09c5d818d66748977c088633e5d5427ad0bc129a
Diwaker Ghimire <idwaker@gmail.com>
2018-01-03 08:43:09 +0545

    added grappelli

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Patient, Doctor, Lab, Clinic, Pharmacy, ClinicTickets
+from .models import (Patient, Doctor, Lab, Clinic, Pharmacy, ClinicTickets,
+                     LabTickets, MedicalHistory, FarmacyTickets)
 
-admin.site.register([Patient, Doctor, Lab, Clinic, Pharmacy, ClinicTickets])
+
+admin.site.register([Patient, Doctor, Lab, Clinic, Pharmacy,
+                     ClinicTickets, LabTickets, MedicalHistory,
+                     FarmacyTickets])
