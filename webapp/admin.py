cb5c9e7bda5987c7bb5747c08fea122c249b0966
Diwaker Ghimire <idwaker@gmail.com>
2018-01-05 10:11:04 +0545

    wip

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,8 +1,11 @@
+
+from django.forms import ModelForm, BaseModelFormSet, formset_factory
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 # Register your models here.
 from .models import (Patient, Doctor, Lab, Clinic, Pharmacy, ClinicTickets,
-                     LabTickets, MedicalHistory, FarmacyTickets)
+                     LabTickets, MedicalHistory, FarmacyTickets, ClinicEmployee)
 
 
 @admin.register(ClinicTickets)
@@ -14,6 +17,45 @@ class ClinicTicketsAdmin(admin.ModelAdmin):
     date_hierarchy = 'assigned_on'
 
 
-admin.site.register([Patient, Doctor, Lab, Clinic, Pharmacy,
+# type('UserFormSet', (BaseModelFormSet,), {'__init__': functiona_name})
+class UserFormSet(BaseModelFormSet):
+    def __init__(self, instance, *args, **kwargs):
+        print('------------------', instance)
+        super().__init__(*args, **kwargs)
+
+
+class UserForm(ModelForm):
+
+    # def __init__(self, **kwargs):
+    #     print(kwargs)
+
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+
+
+# class ClinicEmployeeForm(ModelForm):
+#     class Meta:
+#         model = ClinicEmployee
+
+
+class ClinicEmployeeInline(admin.StackedInline):
+    model = ClinicEmployee
+    # form = ClinicEmployeeForm
+    formset = formset_factory(UserForm, formset=UserFormSet)
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     print(args, kwargs)
+
+
+@admin.register(Clinic)
+class ClinicAdmin(admin.ModelAdmin):
+    inlines = [
+        ClinicEmployeeInline
+    ]
+
+
+admin.site.register([Patient, Doctor, Lab, Pharmacy,
                      LabTickets, MedicalHistory,
                      FarmacyTickets])
